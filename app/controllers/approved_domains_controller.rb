d1fbcec5772588cc9e4359ab2a15820a5e38cb0a
Travis <numbers1311407@gmail.com>
2014-08-14 19:07:15 -0400

    approved domain type filter

--- app/controllers/approved_domains_controller.rb
+++ app/controllers/approved_domains_controller.rb
@@ -1,4 +1,13 @@
 class ApprovedDomainsController < ResourceController
   self.default_sort = {name: 'name', dir: 'asc'}
   permit_params :name
+
+  has_scope :entity do |controller, scope, value|
+    case value
+    when 'mill' then scope = scope.for_mill
+    when 'buyer' then scope = scope.for_buyer
+    end
+
+    scope
+  end
 end
