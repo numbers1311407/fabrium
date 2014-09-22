2e3ca8880a25621c90453f2b4da5f49b71339642
Travis <numbers1311407@gmail.com>
2014-09-22 16:51:45 -0400

    move My Cart button; styles

--- app/assets/javascripts/client.js
+++ app/assets/javascripts/client.js
@@ -5,6 +5,7 @@
 //= require restangular/dist/restangular
 //= require jquery-cycle2/build/jquery.cycle2
 //= require jquery-cycle2/src/jquery.cycle2.carousel
+//= require jquery-cycle2/src/jquery.cycle2.center
 //= require ./ng
 
 // tweak cycle's templating so as not to interfere with angular
