7a9f69b15f53c8686636668b3022035d96bdaa84
Travis <numbers1311407@gmail.com>
2014-10-25 19:38:05 -0400

    bugs

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -14,14 +14,12 @@ class CartItem < ActiveRecord::Base
 
   before_create :denormalize_attrs
 
-  before_save :perform_state_update
-
   protected
 
   # It should be impossible to add items from different mills into a
   # cart created by a Mill
   def ensure_fabric_variant_belongs_to_cart_mill
-    if 'Mill' == cart.creator_type && fabric_variant.mill_id != cart.creator_id
+    if cart.present? && 'Mill' == cart.creator_type && fabric_variant.mill_id != cart.creator_id
       errors.add(:fabric_variant, :cart_mill_mismatch)
     end
   end
@@ -29,9 +27,5 @@ class CartItem < ActiveRecord::Base
   def denormalize_attrs
     self.mill = fabric_variant.mill
     self.fabrium_id = fabric_variant.fabrium_id
-  end
-
-  def perform_state_update
-
   end
 end
